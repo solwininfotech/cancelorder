2f79be184251d6090cc8c25990201fe24f86406e
Mayur Dhudasia <mmdhudasia@gmail.com>
2020-05-12 16:32:18 +0530

    initial

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin CancelOrder Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_CancelOrder
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_CancelOrder',
+    __DIR__
+);
